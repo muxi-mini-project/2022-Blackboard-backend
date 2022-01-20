fbbe863ea75a8eb428ed319748c4a7144de1ecf9
Wishforpeace <90396662+Wishforpeace@users.noreply.github.com>
2022-01-20 11:04:17 +0800

    database

--- db.sql
+++ db.sql
@@ -13,8 +13,8 @@ DROP TABLE IF EXISTS `announcements`;
 --用户信息
 CREATE TABLE `users`(
     `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `student_id` BIGINT NOT NULL,
-    `password` varchar(100) NOT NULL,
+    `student_id` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(100) NOT NULL,
     `nickname` VARCHAR(100) NOT NULL,
     `headportrait` VARCHAR(100) NOT NULL,
     PRIMARY KEY (`id`)
@@ -23,23 +23,23 @@ CREATE TABLE `users`(
 
 CREATE TABLE `collections`{
     `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `student_id` BIGINT NOT NULL,
-    `announcement_id` BIGINT NOT NULL,
+    `student_id` VARCHAR(100) NOT NULL,
+    `announcement_id` VARCHAR(100) NOT NULL,
     `announcement` VARCHAR NULL,
     PRIMARY KEY (`id`)
 }ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `organizations_created`{
     `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `student_id` BIGINT NOT NULL,
-    `org_id` BIGINT NOT NULL,
+    `student_id` VARCHAR(100) NOT NULL,
+    `org_id` VARCHAR(100) NOT NULL,
     `org_name` VARCHAR(100) NOT NULL,
     PRIMARY KEY (`id`)
 }ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `organizations_following`{
     `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `student_id` BIGINT NOT NULL,
+    `student_id` VARCHAR(100) NOT NULL,
     `org_id` VARCHAR(100) NOT NULL,
     `org_name` VARCHAR(100) NOT NULL,
     PRIMARY KEY (`id`)
@@ -57,16 +57,16 @@ CREATE TABLE `organizations`{
 
 CREATE TABLE `groups`{
     `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `org_name` BIGINT NOT NULL,
-    'group_name' VARCHAR NOT NULL,
+    `org_name` VARCHAR(100) NOT NULL,
+    'group_name' VARCHAR(100) NOT NULL,
     PRIMARY KEY(`id`),
     FULLTEXT (`group_name`) WITH PARSER ngram
 }ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `announcements`{
     `id` BIGINT NOT NULL AUTO_INCREMENT,
-    `user_id` BIGINT NOT NULL,
-    `contents` varchar(500) NOT NULL,
+    `user_id` VARCHAR(100) NOT NULL,
+    `contents` VARCHAR(500) NOT NULL,
     PRIMARY KEY (`id`),
     FULLTEXT (`contents`) WITH PARSER ngram
 }ENGINE=InnoDB DEFAULT CHARSET=utf8;
