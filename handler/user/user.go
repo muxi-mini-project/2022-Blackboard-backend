fca284865db1cbff30356e474d6780bb1be205a9
Wishforpeace <90396662+Wishforpeace@users.noreply.github.com>
2022-01-20 22:06:57 +0800

    Partial function realization

--- handler/user/user.go
+++ handler/user/user.go
@@ -1,7 +1,40 @@
 package user
 
-import ()
+import (
+	"blackboard/handler"
+	"blackboard/model"
+	"blackboard/service/user"
+	"encoding/base64"
+	"fmt"
+	"go/token"
 
-func UserInfo() {
+	"github.com/gin-gonic/gin"
+)
 
+//@Summary 用户信息
+//@Tags user
+//@Description 获取用户信息
+func ChangeUserName(c *gin.Context) {
+	token := c.Request.Header.Get("token")
+	id, err := user.VerifyToken(token)
+	if err != nil {
+		fmt.Println(err)
+		c.JSON(401, gin.H{"message": "Token Invalid."})
+		return
+	}
+	var user model.User
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(400, gin.H{"message": "Lack Param Or Param Not Satisfiable."})
+		return
+	}
+
+	user.StudentID = id
+	if user.PassWord != "" {
+		user.PassWord = base64.StdEncoding.EncodeToString([]byte(user.PassWord))
+	}
+	if err := model.ChangeName(user); err != nil {
+		c.JSON(400, gin.H{"message": "更新失败"})
+		return
+	}
+	handler.SendResponse(c, "修改成功", nil)
 }
--- router/router.go
+++ router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	// "blackboard/handler/organization"
+	// "blackboard/handler"
 	"blackboard/handler/user"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -15,46 +16,43 @@ func Router(r *gin.Engine) {
 	g1 := r.Group("/api/v1/user")
 	{
 		//登录
-		g1.POST("/login", user.Login)
-
-		//用户信息
-		g1.GET("", user.UserInfo)
+		g1.POST("", user.Login)
 
 		//修改用户名
-		g1.PUT("", user.ChangeUserName)
+		g1.PUT("/changename", user.ChangeUserName)
 
-		//修改用户头像
-		g1.PUT("", user.ChangeHeadportrait)
+		// //修改用户头像
+		// g1.PUT("", user.ChangeHeadportrait)
 
-		//用户收藏
-		g1.GET("/collection/:id", user.UserCollection)
+		// //用户收藏
+		// g1.GET("/collection/:id", user.UserCollection)
 
-		//用户发布
-		g1.GET("/published/:id", user.UserPublish)
+		// //用户发布
+		// g1.GET("/published/:id", user.UserPublish)
 
 	}
 
-	//organizations
-	g2 := r.Group("/api/v1/organization")
-	{
-		//查看创建的组织
-		g2.GET("/personal/:user_id/created", organization.CheckCreated)
-		//查看关注的组织
-		g2.GET("/personal/:user_id/following", organization.CheckFollowing)
-		//查看特定组织信息
-		g2.GET("/personal/:user_id/created/:organization_id", organization.CheckDetails)
-		g2.GET("/personal/:user_id/following/:organization_id", organization.CheckDetails)
-	}
-	//announcement
-	g3 := r.Group("/api/v1/announcement")
-	{
-		//查看最新通知
-		g3.GET("", announcement.CheckAllPubilshed)
-		//发布通知
-		g3.POST("/:user_id/publish/:organization_id", announcement.PublishNews)
-		//删除通知
-		g3.DELETE("/:user_id/delete/:organization_id", announcement.DeletePublished)
-		//收藏通知
-		g3.POST("/:user_id/collect/:organization_id/:announcement_id", announcement.Collect)
-	}
+	// //organizations
+	// g2 := r.Group("/api/v1/organization")
+	// {
+	// 	//查看创建的组织
+	// 	g2.GET("/personal/:user_id/created", organization.CheckCreated)
+	// 	//查看关注的组织
+	// 	g2.GET("/personal/:user_id/following", organization.CheckFollowing)
+	// 	//查看特定组织信息
+	// 	g2.GET("/personal/:user_id/created/:organization_id", organization.CheckDetails)
+	// 	g2.GET("/personal/:user_id/following/:organization_id", organization.CheckDetails)
+	// }
+	// //announcement
+	// g3 := r.Group("/api/v1/announcement")
+	// {
+	// 	//查看最新通知
+	// 	g3.GET("", announcement.CheckAllPubilshed)
+	// 	//发布通知
+	// 	g3.POST("/:user_id/publish/:organization_id", announcement.PublishNews)
+	// 	//删除通知
+	// 	g3.DELETE("/:user_id/delete/:organization_id", announcement.DeletePublished)
+	// 	//收藏通知
+	// 	g3.POST("/:user_id/collect/:organization_id/:announcement_id", announcement.Collect)
+	// }
 }
